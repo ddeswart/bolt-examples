5877b758d658593588e20b1d71a5d2f838d34fe4
Dimitri <ddeswart@users.noreply.github.com>
2019-01-07 11:40:16 +0100

    Deleted password

--- Boltdir/modules/epops/tasks/ep_install.sh
+++ Boltdir/modules/epops/tasks/ep_install.sh
@@ -8,7 +8,7 @@ echo "Create install directory and download agent software"
 cd /opt/
 mkdir $PT_installdir
 cd $PT_installdir/
-wget -c https://10.0.0.140:5001/software_repo/epops-linux-7.0.tar.gz --no-check-certificate --user vmguru --password VMguru123!
+wget -c https://10.0.0.140:5001/software_repo/epops-linux-7.0.tar.gz --no-check-certificate --user xxxxxx --password xxxxxx
 #
 echo "Unpack agent software"
 tar zxfp epops-linux-7.0.tar.gz
