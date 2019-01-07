a4cbb3349ba88e1cf6427a1950b1702e7cc3a8b8
Dimitri <ddeswart@users.noreply.github.com>
2019-01-07 11:42:42 +0100

    Delete test.pp

--- Boltdir/modules/epops/plans/test.pp
+++
@@ -1,9 +0,0 @@
-plan epops::test(
-  TargetSpec $nodes,
-  String     $installdir = "vmware",
-) {
-  $result = run_task("epops::ep_check", $nodes)
-  notice("Result: ${result}")
-  $filtered_result = $result.filter | $output | {$output =~ "EPOps install directory found"}
-  notice("${filtered_result}")
-}
