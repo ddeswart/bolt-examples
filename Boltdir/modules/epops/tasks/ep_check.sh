666391ba3dfb645c6d1c8f42c31e667b0cb5555c
Dimitri <ddeswart@users.noreply.github.com>
2019-01-14 14:07:24 +0100

    Update ep_check.sh

--- Boltdir/modules/epops/tasks/ep_check.sh
+++ Boltdir/modules/epops/tasks/ep_check.sh
@@ -3,18 +3,19 @@
 if [[ -n $(find /opt/ -type d -name "epops*") ]]
 then
   echo "EPOps install directory found"
-  if [[ -n $(service epops-agent status | grep "is not running") ]]
+  if [[ -n $(systemctl list-units --type service --all | grep "epops-agent") ]]
   then
-    echo "EPOps agent is NOT running"
-    echo "Trying to start agent..."
-    if [[ -n $(systemctl list-units --type service --all | grep "epops-agent") ]]
+    if [[ -n $(service epops-agent status | grep "is not running") ]]
     then
+      echo "EPOps agent is NOT running"
+      echo "Trying to start agent..."
       service epops-agent start
     else
-      echo "EPOps service not installed, try to start agent from epops/bin directory manually"
+      echo "EPOps agent is running"
+      exit 0
     fi  
   else
-    echo "EPOps agent is running"
+    echo "EPOps agent service is NOT installed, try to start/stop it manually from EPOps bin directory"
     exit 0
   fi
 else
