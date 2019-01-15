1ead1747f520935f44d15d8c0a22a59751a749c5
Dimitri <ddeswart@users.noreply.github.com>
2019-01-15 21:01:29 +0100

    Update epops70_linux.pp

--- Boltdir/modules/epops/plans/epops70_linux.pp
+++ Boltdir/modules/epops/plans/epops70_linux.pp
@@ -9,10 +9,10 @@ plan epops::epops70_linux(
       notice("Info: Node '${node}' already contains the EPOps Agent, skipping.")
     } else {
       notice("Info: Node '${node}' requires installation of the EPOps Agent, continuing")
-      run_task('epops::ep_install', $nodes, installdir => $installdir)
-      run_command("rm /opt/${installdir}/epops-agent-7.0.0/conf/agent.properties --force", $nodes)
-      upload_file('epops/lnx-agent.properties', "/opt/${installdir}/epops-agent-7.0.0/conf/agent.properties", $nodes)
-      run_task('epops::ep_start', $nodes, installdir => $installdir)
+      run_task('epops::ep_install', $node, installdir => $installdir)
+      run_command("rm /opt/${installdir}/epops-agent-7.0.0/conf/agent.properties --force", $node)
+      upload_file('epops/linux-agent.properties', "/opt/${installdir}/epops-agent-7.0.0/conf/agent.properties", $node)
+      run_task('epops::ep_start', $node, installdir => $installdir)
     }
   }
 }
